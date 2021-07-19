80fb91e4e09ba329d49ca02a8a3019127683b62a
Darek Mazur <dev@darekmazur.it>
2021-07-19 20:01:18 +0200

    refactor renderElement method

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,5 +1,5 @@
-import { renderElement } from '../../utils/helpers';
-import { getStationsList } from '../../utils/helpers';
+import { getCitiesByProvince, renderElement } from '../../utils/helpers';
+import { getProvincesList } from '../../utils/helpers';
 
 const Main = () => {
   // const mainWrapper = document.createElement('main');
@@ -9,17 +9,34 @@ const Main = () => {
 
   // document.querySelector('body').appendChild(mainWrapper);
   // mainWrapper.appendChild(mainForm);
-  renderElement('main', 'main__wrapper', document.querySelector('body'));
-  renderElement('form', 'form__wrapper', document.querySelector('.main__wrapper'));
-  renderElement('select', 'form__select', document.querySelector('.form__wrapper'));
+  renderElement('main', ['main__wrapper'], document.querySelector('body'));
+  renderElement('form', ['form__wrapper'], document.querySelector('.main__wrapper'));
+  renderElement(
+    'select',
+    ['form__select', 'selcet__province'],
+    document.querySelector('.form__wrapper'),
+  );
 
-  // renderElement('option', 'form__option', document.querySelector('.form__select'))
-  // getStationsList().then((response) =>
-  //   response.map((station) => {
-  //     renderElement('option', null, document.querySelector('.form__select'), station);
-  //   }),
-  // );
-  console.log(getStationsList());
+  renderElement('option', [], document.querySelector('.selcet__province'), 'Choose your province:');
+  getProvincesList().then((response) =>
+    response.map((province) => {
+      renderElement('option', [], document.querySelector('.selcet__province'), province);
+    }),
+  );
+
+  renderElement(
+    'select',
+    ['form__select', 'selcet__city'],
+    document.querySelector('.form__wrapper'),
+  );
+  renderElement('option', [], document.querySelector('.selcet__city'), 'Choose your city:');
+  getCitiesByProvince('ŚWIĘTOKRZYSKIE').then((response) =>
+    response.map((city) => {
+      renderElement('option', [], document.querySelector('.selcet__city'), city);
+    }),
+  );
+
+  // console.log(getCitiesByProvince('ŚWIĘTOKRZYSKIE'));
 };
 
 export default Main;
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,18 +7,37 @@ const getData = fetch(API_URL)
   .then((data) => data)
   .catch((error) => console.log(error));
 
-export const getStationsList = async () => {
-  const allStationsData = await getData;
-  const stations = [];
-  allStationsData.forEach((station) =>
-    !stations.includes(station.city.name) ? stations.push(station.city.name) : null,
+//TODO: get...List method: one method for all steps, add 'parent' prop;
+
+export const getProvincesList = async () => {
+  const allProvincesData = await getData;
+  const provinces = [];
+  allProvincesData.forEach((province) =>
+    !provinces.includes(province.city.commune.provinceName)
+      ? provinces.push(province.city.commune.provinceName)
+      : null,
   );
-  return stations;
+  return provinces;
+};
+
+export const getCitiesByProvince = async (province) => {
+  const allCitiesData = await getData;
+  const cities = [];
+  allCitiesData.forEach((city) =>
+    !cities.includes(city.city.name) && city.city.commune.provinceName === province
+      ? cities.push(city.city.name)
+      : null,
+  );
+  return cities;
 };
 
 export const renderElement = (tag, className, target, content) => {
+  //TODO: clasName as Array; if length = 0 then skip classList.add; if length = 1 then classList.add; if length > 1 then forEach classList.add
   const newElement = document.createElement(tag);
-  newElement.classList.add(className);
+  if (className.length > 0) {
+    className.forEach((singleClass) => newElement.classList.add(singleClass));
+  }
+
   if (content) {
     newElement.innerHTML = content;
   }
