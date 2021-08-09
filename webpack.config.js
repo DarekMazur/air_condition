345d9df32c95c725973da118c1d3f324dd324cb1
Darek Mazur <dev@darekmazur.it>
2021-08-09 20:01:54 +0200

    config google fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
-const path = require("path");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    publicPath: "/dist/",
-    filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 
   module: {
@@ -14,27 +17,89 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  useBuiltIns: "entry",
-                },
-              ],
-            ],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
-        test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.s(c|a)ss$/,
+        use: {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: './',
+          },
+        },
       },
       {
+        test: /\.s(c|a)ss$/,
+        use: {
+          loader: 'css-loader',
+        },
+      },
+      {
+        test: /\.s(c|a)ss$/,
+        use: {
+          loader: 'sass-loader',
+        },
+      },
+
+      {
         test: /\.(png|jpg|gif|svg)/,
-        type: "asset/resource",
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      // template: './src/index.html',
+      title: 'Air condition',
+      inject: 'body',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1.0',
+        description: 'Check air condition in your area',
+      },
+    }),
+    new HtmlWebpackTagsPlugin({
+      links: [
+        {
+          append: false,
+          path: 'https://fonts.googleapis.com',
+          attributes: {
+            rel: 'preconnect',
+          },
+        },
+        {
+          append: false,
+          path: 'https://fonts.gstatic.com',
+          attributes: {
+            rel: 'preconnect',
+            crossorigin: true,
+          },
+        },
+        {
+          append: false,
+          path: 'https://fonts.googleapis.com/css2?family=Indie+Flower&family=Montserrat&family=Oswald:wght@500&display=swap',
+          attributes: {
+            rel: 'stylesheet',
+            crossorigin: 'anonymous',
+          },
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 };
