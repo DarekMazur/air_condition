6b5ddf175087b6cd1cf2f194defe0f24ffd3b243
Darek Mazur <dev@darekmazur.it>
2021-08-12 23:33:51 +0200

    add russian to setInitialLanguage method

--- src/Helpers/setInitialLanguage.js
+++ src/Helpers/setInitialLanguage.js
@@ -2,7 +2,11 @@ export const setInitialLanguage = () => {
   let lang = '';
 
   if (window.localStorage.length === 0) {
-    navigator.language === 'pl-PL' ? (lang = 'PL') : (lang = 'EN');
+    navigator.language === 'pl-PL'
+      ? (lang = 'PL')
+      : navigator.language === 'ru-RU'
+      ? (lang = 'RU')
+      : (lang = 'EN');
     window.localStorage.setItem('lang', lang);
   }
 };
